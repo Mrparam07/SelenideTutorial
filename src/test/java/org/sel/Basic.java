40aac93d7c4c0287403a8af63d3ac9f5049c68f4
PARAMVEER SINGH <rapgodparam7599@gmail.com>
2024-02-25 12:33:40 +0530

    #comment 001

--- src/test/java/org/sel/Basic.java
+++ src/test/java/org/sel/Basic.java
@@ -1,2 +1,22 @@
-package org.sel;public class Basic {
+package org.sel;
+
+
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class Basic {
+    @Test
+    public void googleSearch() throws InterruptedException {
+        open("https://www.google.com/");
+//        $(By.name("q")).sendKeys("Parambir Kumar");
+//        $(By.name("btnK")).pressEnter();
+//        $(By.id("logo")).shouldHave(appear);
+          // Perform the search
+        $("[name='q']").setValue("Parambir Kumar").pressEnter();
+          // Wait for the search results to appear
+        $("#search").shouldBe(Condition.visible);
+    }
 }
